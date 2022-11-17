58d48940594e2be6e1bb7289fa2865b98c762b99
Miracle_KK <425710382@qq.com>
2022-11-17 22:46:23 +0800

    HaoYunLai项目初始化

---
+++ Source/HaoYunLai/HaoYunLaiGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HaoYunLaiGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAOYUNLAI_API AHaoYunLaiGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
